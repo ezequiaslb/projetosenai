e344dfa52d629ed87a29237e992328e449f6e6ec
ezequiaslb <ezequiaslb@users.noreply.github.com>
2023-03-23 14:29:05 -0300

    1 dia

---
+++ conecta.php
@@ -0,0 +1,19 @@
+<?php
+// Definir as variáveis para conexão com o banco de dados
+$hostname = "localhost";   // Endereço do servidor de banco de dados
+$username = "ezequias"; // Nome do usuário do banco de dados
+$password = "[SYLlmSDrngBRroi";   // Senha do usuário do banco de dados
+$dbname   = "colecoes_pessoais";  // Nome do banco de dados
+
+// Conectar ao banco de dados usando a função mysqli_connect()
+$conexao = mysqli_connect($hostname, $username, $password, $dbname);
+
+// Verificar se houve erros na conexão
+if (mysqli_connect_errno()) {
+    die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
+}
+else{
+    echo("Conexão feita com Sucesso!");
+}
+
+?>
---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Sistema de Reservas de Espaços</title>
+</head>
+<body>
+<div class="container">
+      <form action="valida_login.php" method="post">
+        <h2>Login</h2>
+        <label for="username">Usuário:</label>
+        <input type="text" name="username" id="username" required>
+        <label for="password">Senha:</label>
+        <input type="password" name="password" id="password" required>
+        <input type="submit" value="Entrar">
+      </form>
+    </div>
+</body>
+</html>
