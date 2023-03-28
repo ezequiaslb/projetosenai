a7e4da26a38bbed78cbabce22c4d0a4185f9fddd
ezequiaslb <ezequiaslb@users.noreply.github.com>
2023-03-28 01:39:48 -0300

    eita

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -25,24 +25,28 @@
     <div class="container">
         <h1>Cadastro de Usuários</h1>
 
-        <form  action="processa_cadastro_usuario.php" method="POST">
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" required><br>
+        <form method="POST" action="processa_cadastro_usuario.php">
+	<label for="nome">Nome:</label>
+	<input type="text" id="nome" name="nome" required>
 
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required><br>
+	<label for="email">E-mail:</label>
+	<input type="email" id="email" name="email" required>
 
-            <label for="telefone">Telefone:</label>
-            <input type="text" id="telefone" name="telefone"><br>
+	<label for="telefone">Telefone:</label>
+	<input type="text" id="telefone" name="telefone">
 
-            <label for="senha">Senha:</label>
-            <input type="password" id="senha" name="senha" required><br>
+	<label for="senha">Senha:</label>
+	<input type="password" id="senha" name="senha" required>
 
-            <label for="confirmar_senha">Confirmar senha:</label>
-            <input type="password" id="confirmar_senha" name="confirmar_senha" required><br>
+	<label for="tipo_usuario">Tipo de Usuário:</label>
+	<select id="tipo_usuario" name="tipo_usuario">
+		<option value="1">Administrador</option>
+		<option value="2" selected>Usuário Padrão</option>
+	</select>
 
-            <input type="submit" value="Cadastrar">
-    </div>
-</form>
+	<input type="submit" value="Cadastrar">
+    </form>
+</div>
+
 </body>
 </html>
--- processa_cadastro_usuario.php
+++ processa_cadastro_usuario.php
@@ -5,11 +5,40 @@ if(!isset($_SESSION['id_usuario'])) {
 	// Redireciona o usuário para a página de login
 	header("Location: login.php");
 } 
-require_once "conecta.php";
+include_once "conecta.php";
 
+// Recebe os dados do formulário de cadastro
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$senha = $_POST['senha'];
+$tipo_usuario = $_POST['tipo_usuario'];
 
+// Valida os dados recebidos
+if(empty($nome) || empty($email) || empty($senha)) {
+    header("Location: cadastro_usuario.php?erro=1");
+    exit();
+}
 
+// Verifica se o e-mail já está cadastrado
+$sql = "SELECT id FROM usuarios WHERE email = '$email'";
+$resultado = mysqli_query($conexao, $sql);
 
+if(mysqli_num_rows($resultado) > 0) {
+    header("Location: cadastro_usuario.php?erro=2");
+    exit();
+}
+
+// Insere o novo registro na tabela de usuários
+$sql = "INSERT INTO usuarios (nome, email, telefone, senha, tipo_usuario) VALUES ('$nome', '$email', '$telefone', '$senha', $tipo_usuario)";
+    
+if(mysqli_query($conexao, $sql)) {
+    header("Location: cadastro_usuario.php?msg=cadastro_sucesso");
+    exit();
+} else {
+    header("Location: cadastro_usuario.php?erro=3");
+    exit();
+}
 
 
 ?>
