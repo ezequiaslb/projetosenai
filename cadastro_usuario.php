a6ee4df220b6fbef53f47592f980c76a98bccabc
ezequiaslb <ezequiaslb@users.noreply.github.com>
2023-03-28 01:48:27 -0300

    Update cadastro_usuario.php

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -1,3 +1,9 @@
+<?php 
+if (!isset($_SESSION['cadastro_sucesso']) || $_SESSION['cadastro_sucesso'] !== true) {
+    header("Location: admin.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
