c14ee354c07938edd404dbf8c2b96f2356f428ff
ezequiaslb <ezequiaslb@users.noreply.github.com>
2023-04-15 00:27:15 -0300

    eita

--- user.php
+++ user.php
@@ -1,3 +1,65 @@
-<?php 
-include 'conecta.php';
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Reservas - Usuário</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Sistema de Reservas</h1>
+        <nav>
+            <ul>
+                <li><a href="user.php">Minhas Reservas</a></li>
+                <li><a href="fazer_reserva.php">Fazer Reserva</a></li>
+                <li><a href="logout.php">Sair</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h2>Minhas Reservas</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Data de início</th>
+                    <th>Data de término</th>
+                    <th>Espaço</th>
+                    <th>Equipamento</th>
+                    <th>Status</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                require 'conecta.php';
+                session_start();
+                $id_usuario = $_SESSION['id_usuario'];
+                $sql = "SELECT r.id, r.data_inicio, r.data_termino, e.nome AS espaco, q.nome AS equipamento, r.status FROM reservas r JOIN espacos e ON r.id_espaco = e.id
+                LEFT JOIN equipamentos q ON r.id_equipamento = q.id
+                        WHERE r.id_usuario = $id_usuario";
+                $resultado = mysqli_query($conexao, $sql);
+                while ($reserva = mysqli_fetch_assoc($resultado)) {
+                    $status = $reserva['status'] == 1 ? 'Ativa' : 'Cancelada';
+                ?>
+                    <tr>
+                        <td><?= date('d/m/Y H:i', strtotime($reserva['data_inicio'])) ?></td>
+                        <td><?= date('d/m/Y H:i', strtotime($reserva['data_termino'])) ?></td>
+                        <td><?= $reserva['espaco'] ?></td>
+                        <td><?= $reserva['equipamento'] ?? '-' ?></td>
+                        <td><?= $status ?></td>
+                        <td>
+                            <?php if ($reserva['status'] == 1 && strtotime($reserva['data_inicio']) > time()) { ?>
+                                <form action="excluir_reserva.php" method="post">
+                                    <input type="hidden" name="id_reserva" value="<?= $reserva['id'] ?>">
+                                    <button type="submit">Excluir</button>
+                                </form>
+                            <?php } ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </main>
+</body>
+</html>
--- valida_login.php
+++ valida_login.php
@@ -1,47 +1,95 @@
 <?php
+// session_start();
+
+// // Definir as variáveis para conexão com o banco de dados
+// $hostname = "localhost";   // Endereço do servidor de banco de dados
+// $username = "ezequias"; // Nome do usuário do banco de dados
+// $password = "[SYLlmSDrngBRroi";   // Senha do usuário do banco de dados
+// $dbname   = "sistemareservas";  // Nome do banco de dados
+
+// // Conectar ao banco de dados usando a função mysqli_connect()
+// $conexao = mysqli_connect($hostname, $username, $password, $dbname);
+
+// // Verificar se houve erros na conexão
+// if (mysqli_connect_errno()) {
+//     die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
+// }
+
+// // Receber os dados do formulário
+// $email = $_POST['email'];
+// $senha = $_POST['senha'];
+
+// // Preparar a consulta SQL para buscar o usuário no banco de dados
+// $sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+
+// // Executar a consulta SQL
+// $resultado = mysqli_query($conexao, $sql);
+
+// // Verificar se a consulta retornou algum resultado
+// if (mysqli_num_rows($resultado) > 0) {
+//     // O usuário foi encontrado, verificar se é um administrador ou não
+//     $usuario = mysqli_fetch_assoc($resultado);
+//     if ($usuario['tipo_usuario'] == 1) {
+//         // O usuário é um administrador, redirecionar para a página de administração
+//         $_SESSION['admin'] = true;
+//         header('Location: painel_adm.php');
+//     } else {
+//         // O usuário é um usuário comum, redirecionar para a página de usuário
+//         $_SESSION['admin'] = false;
+//         header('Location: user.php');
+//     }
+// } else {
+//     // O usuário não foi encontrado, exibir mensagem de erro e redirecionar para a página de login
+//     $_SESSION['mensagem'] = 'E-mail ou senha incorretos';
+//     header('Location: index.php');
+// }
+
+// Inclui o arquivo de conexão com o banco de dados
+include 'conecta.php';
+
+// Inicia a sessão do usuário
 session_start();
 
-// Definir as variáveis para conexão com o banco de dados
-$hostname = "localhost";   // Endereço do servidor de banco de dados
-$username = "ezequias"; // Nome do usuário do banco de dados
-$password = "[SYLlmSDrngBRroi";   // Senha do usuário do banco de dados
-$dbname   = "sistemareservas";  // Nome do banco de dados
+// Verifica se o formulário foi enviado
+if(isset($_POST['email']) && isset($_POST['senha'])) {
+    // Atribui os valores dos campos de email e senha para as variáveis
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
-// Conectar ao banco de dados usando a função mysqli_connect()
-$conexao = mysqli_connect($hostname, $username, $password, $dbname);
+    // Prepara a consulta SQL para buscar o usuário no banco de dados
+    $consulta = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
 
-// Verificar se houve erros na conexão
-if (mysqli_connect_errno()) {
-    die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
-}
+    // Executa a consulta no banco de dados
+    $resultado = mysqli_query($conexao, $consulta);
 
-// Receber os dados do formulário
-$email = $_POST['email'];
-$senha = $_POST['senha'];
+    // Verifica se foi encontrado um usuário com os dados informados
+    if(mysqli_num_rows($resultado) == 1) {
+        // Armazena os dados do usuário em um array associativo
+        $usuario = mysqli_fetch_assoc($resultado);
 
-// Preparar a consulta SQL para buscar o usuário no banco de dados
-$sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        // Cria uma variável de sessão com o id do usuário
+        $_SESSION['id_usuario'] = $usuario['id'];
 
-// Executar a consulta SQL
-$resultado = mysqli_query($conexao, $sql);
-
-// Verificar se a consulta retornou algum resultado
-if (mysqli_num_rows($resultado) > 0) {
-    // O usuário foi encontrado, verificar se é um administrador ou não
-    $usuario = mysqli_fetch_assoc($resultado);
-    if ($usuario['tipo_usuario'] == 1) {
-        // O usuário é um administrador, redirecionar para a página de administração
-        $_SESSION['admin'] = true;
-        header('Location: painel_adm.php');
+        // Verifica o tipo de usuário (administrador ou usuário padrão)
+        if($usuario['tipo_usuario'] == 1) {
+            // Se for um administrador, redireciona para a página do painel administrativo
+            header('Location: painel_adm.php');
+            exit;
+        } else {
+            // Se for um usuário padrão, redireciona para a página do usuário
+            header('Location: user.php');
+            exit;
+        }
     } else {
-        // O usuário é um usuário comum, redirecionar para a página de usuário
-        $_SESSION['admin'] = false;
-        header('Location: user.php');
+        // Caso contrário, exibe uma mensagem de erro e redireciona para a página de login
+        $_SESSION['mensagem'] = 'Email ou senha inválidos';
+        header('Location: index.php');
+        exit;
     }
 } else {
-    // O usuário não foi encontrado, exibir mensagem de erro e redirecionar para a página de login
-    $_SESSION['mensagem'] = 'E-mail ou senha incorretos';
-    header('Location: index.php');
+    // Caso o formulário não tenha sido enviado, redireciona para a página de login
+    header('Location: login.php');
+    exit;
 }
 
 // Fechar a conexão com o banco de dados
